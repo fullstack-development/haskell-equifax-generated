d37920e77b3bb6716348b8138360a490164e67d4
znack <scher56@gmail.com>
2021-07-13 13:23:27 +0700

    make originalAmount and balance double

--- lib/ConsumerCreditReport/ModelLens.hs
+++ lib/ConsumerCreditReport/ModelLens.hs
@@ -1,7 +1,7 @@
 {-
    Consumer Credit Report
 
-   # Introduction       Equifax’s Consumer Credit Report is the leading consumer credit resource enabling lenders to make faster, more informed credit-granting decisions, better manage their risk and maximize growth opportunities. The Consumer Credit Report unites the power of superior consumer dfata with best-in-class search and match logic capabilities to deliver higher match rates on more inquiries.    # Getting Started  1. **<a href=\"/user/applications\" target=\"_blank\">Create</a>** an application  2. **<a href=\"/user/applications\" target=\"_blank\">Subscribe</a>** to Consumer Credit Report API  3. **<a href=\"/products/consumer-credit-report\" target=\"_blank\">Explore</a>** the Sandbox environment mode # Promoting to UAT  To successfully submit test transactions through your application in the UAT environment, a test member number is required.  You may use your existing Equifax test member number(s).  Please contact your Equifax Account Representative if a new test member number is needed. The steps below can be used to test in the UAT environment.  Transactions generated using a test member number do not return identical results as a sandbox transaction.  A sandbox transaction will reflect everything that is exposed in the API whereas a test member number is configured to align with the specifications of your contract. Please contact us to ensure your test member number is configured to your expectations.      1. **<a href=\"/user/applications\" target=\"_blank\">Promote</a>** application to test  2. **<a href=\"/documentation\" target=\"_blank\">Update</a>** requests details for UAT  3. **<a href=\"/documentation\" target=\"_blank\">Test</a>** scenarios in UAT environment   # For More Details    * **<a href=\"/contact\" target=\"_blank\">Contact Us</a>** - Equifax Developer Center Support Website   * **ACRO Migration Support**       * phone: 1-888-407-0359; Option 2 followed by Option 5         * email:  BT.Acro.tech@equifax.com        
+   # Introduction       Equifax’s Consumer Credit Report is the leading consumer credit resource enabling lenders to make faster, more informed credit-granting decisions, better manage their risk and maximize growth opportunities. The Consumer Credit Report unites the power of superior consumer dfata with best-in-class search and match logic capabilities to deliver higher match rates on more inquiries.    # Getting Started  1. **<a href=\"/user/applications\" target=\"_blank\">Create</a>** an application  2. **<a href=\"/user/applications\" target=\"_blank\">Subscribe</a>** to Consumer Credit Report API  3. **<a href=\"/products/consumer-credit-report\" target=\"_blank\">Explore</a>** the Sandbox environment mode # Promoting to UAT  To successfully submit test transactions through your application in the UAT environment, a test member number is required.  You may use your existing Equifax test member number(s).  Please contact your Equifax Account Representative if a new test member number is needed. The steps below can be used to test in the UAT environment.  Transactions generated using a test member number do not return identical results as a sandbox transaction.  A sandbox transaction will reflect everything that is exposed in the API whereas a test member number is configured to align with the specifications of your contract. Please contact us to ensure your test member number is configured to your expectations.      1. **<a href=\"/user/applications\" target=\"_blank\">Promote</a>** application to test  2. **<a href=\"/documentation\" target=\"_blank\">Update</a>** requests details for UAT  3. **<a href=\"/documentation\" target=\"_blank\">Test</a>** scenarios in UAT environment   # For More Details    * **<a href=\"/contact\" target=\"_blank\">Contact Us</a>** - Equifax Developer Center Support Website   * **ACRO Migration Support**       * phone: 1-888-407-0359; Option 2 followed by Option 5         * email:  BT.Acro.tech@equifax.com
 
    OpenAPI Version: 3.0.0
    Consumer Credit Report API version: 1.0.0
@@ -471,7 +471,7 @@ collectionDateAssignedL f Collection{..} = (\collectionDateAssigned -> Collectio
 {-# INLINE collectionDateAssignedL #-}
 
 -- | 'collectionOriginalAmount' Lens
-collectionOriginalAmountL :: Lens_' Collection (Maybe Text)
+collectionOriginalAmountL :: Lens_' Collection (Maybe Double)
 collectionOriginalAmountL f Collection{..} = (\collectionOriginalAmount -> Collection { collectionOriginalAmount, ..} ) <$> f collectionOriginalAmount
 {-# INLINE collectionOriginalAmountL #-}
 
@@ -481,7 +481,7 @@ collectionStatusDateL f Collection{..} = (\collectionStatusDate -> Collection { 
 {-# INLINE collectionStatusDateL #-}
 
 -- | 'collectionBalance' Lens
-collectionBalanceL :: Lens_' Collection (Maybe Text)
+collectionBalanceL :: Lens_' Collection (Maybe Double)
 collectionBalanceL f Collection{..} = (\collectionBalance -> Collection { collectionBalance, ..} ) <$> f collectionBalance
 {-# INLINE collectionBalanceL #-}
 
